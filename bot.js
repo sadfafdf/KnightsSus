c0aeb6bf53b490b136e3a388e412a5c63a951680
sadfafdf <52762381+sadfafdf@users.noreply.github.com>
2019-07-10 23:42:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,6 +34,58 @@ var ti={}
 ,attentions={};
 
 
+
+client.on('message', message => {
+
+     if (message.author.bot) return;
+
+    if (message.content.startsWith("رابط")) {
+
+        message.channel.createInvite({
+
+        thing: true,
+
+        maxUses: 10,
+
+        maxAge: 36000,
+
+    }).then(invite =>
+
+      message.author.sendMessage(invite.url)
+
+    )
+
+    const embed = new Discord.RichEmbed()
+
+        .setColor("RANDOM")
+
+          .setDescription(" تم أرسال الرابط برسالة خاصة ")
+
+           .setAuthor(client.user.username, client.user.avatarURL)
+
+                 .setAuthor(client.user.username, client.user.avatarURL)
+
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+ 
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+
+              const Embed11 = new Discord.RichEmbed()
+
+        .setColor("RANDOM")
+
+       
+
+    .setDescription(" مدة الرابط : يوم  عدد استخدامات الرابط : 10 ")
+
+      message.author.sendEmbed(Embed11)
+
+    }
+ 
+});
+
+
 client.on('message', message => {
   if(message.author.bot) return;
   if(message.content.startsWith(prefix + 'new')) {
